ede0c9dbfe114eca54948da07c605bc225c6dd59
shakibdshy <shakibdshy@gmail.com>
2022-04-17 03:03:10 +0600

    Service Section Update

--- src/Components/Service/Service.js
+++ src/Components/Service/Service.js
@@ -1,17 +1,20 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import {
   FaAngleDoubleRight,
   FaDumbbell,
-  FaAccessibleIcon,
 } from "react-icons/fa";
 import ServiceBG from "../../images/service-bg-1.jpg";
-import Service1 from "../../images/service-1.jpg";
-import Service2 from "../../images/service-2.jpg";
-import Service3 from "../../images/service-3.jpg";
 import { Link } from "react-router-dom";
 import "./Service.css";
 
 const Service = () => {
+  const [Services, setService] = useState([]);
+
+  useEffect(() => {
+    fetch("service.json")
+      .then((response) => response.json())
+      .then((json) => setService(json));
+  }, []);
   return (
     <section
       className='service-area'
@@ -30,66 +33,31 @@ const Service = () => {
           </div>
         </div>
         <div className='row'>
-          <div className='col-md-6 col-lg-4'>
-            <div className='service-item service-item-2'>
-              <div className='service-img'>
-                <img src={Service1} alt='Service' />
-              </div>
-              <div className='service-content service-content-2 text-center white-color'>
-                <div className='service-icon'>
-                  <FaDumbbell />
+          {Services.map((service) => {
+            return (
+              <div className='col-md-6 col-lg-4' key={service.id}>
+                <div className='service-item service-item-2'>
+                  <div className='service-img'>
+                    <img src={service.image} alt='Service' />
+                  </div>
+                  <div className='service-content service-content-2 text-center white-color'>
+                    <div className='service-icon'>
+                      <FaDumbbell />
+                    </div>
+                    <h3 className='white-color'>{service.name}</h3>
+                    <p>
+                      Avoids pleasure itself, because it is plea sure but
+                      because those who do not know how to pursue pleasure
+                      rationally
+                    </p>
+                    <Link to='/checkout'>
+                      <FaAngleDoubleRight />
+                    </Link>
+                  </div>
                 </div>
-                <h3 className='white-color'>Dumbbelling</h3>
-                <p>
-                  Avoids pleasure itself, because it is plea sure but because
-                  those who do not know how to pursue pleasure rationally
-                </p>
-                <Link to='/checkout'>
-                  <FaAngleDoubleRight />
-                </Link>
               </div>
-            </div>
-          </div>
-          <div className='col-md-6 col-lg-4'>
-            <div className='service-item service-item-2'>
-              <div className='service-img'>
-                <img src={Service2} alt='Services' />
-              </div>
-              <div className='service-content service-content-2 text-center white-color'>
-                <div className='service-icon'>
-                  <FaAccessibleIcon />
-                </div>
-                <h3 className='white-color'>Cycling GYM</h3>
-                <p>
-                  Avoids pleasure itself, because it is plea sure but because
-                  those who do not know how to pursue pleasure rationally
-                </p>
-                <Link to='/checkout'>
-                  <FaAngleDoubleRight />
-                </Link>
-              </div>
-            </div>
-          </div>
-          <div className='col-md-6 col-lg-4'>
-            <div className='service-item service-item-2'>
-              <div className='service-img'>
-                <img src={Service3} alt='Services' />
-              </div>
-              <div className='service-content service-content-2 text-center white-color'>
-                <div className='service-icon'>
-                  <FaDumbbell />
-                </div>
-                <h3 className='white-color'>Grid Training</h3>
-                <p>
-                  Avoids pleasure itself, because it is plea sure but because
-                  those who do not know how to pursue pleasure rationally
-                </p>
-                <Link to='/checkout'>
-                  <FaAngleDoubleRight />
-                </Link>
-              </div>
-            </div>
-          </div>
+            );
+          })}
         </div>
       </div>
     </section>
